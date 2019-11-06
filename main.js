e3b1dbb32194790e2be2d726a368370d2774d0d0
Ashok Kumar Yadav <ashokkumar.y@Ashok-261.local>
2019-11-06 16:00:12 +0530

    keyboard accessibility

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const prev_mth_element = document.querySelector('.date-picker .dates .months .pr
 
 const days_element = document.querySelector('.date-picker .days');
 const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+const daysOfMonth = ["sun","mon","tue","wed","thus","fri","sat"];
 
 let date = new Date();
 let day = date.getDate();
@@ -17,12 +18,12 @@ let year = date.getFullYear();
 let selected_day = day;
 let selected_month = month;
 let selected_year = year;
-
-
+var reqIndex;
+var  previousSelectedDate;
 mth_element.textContent =  months[month] + ' ' + year;
 
 selected_date_element.textContent = formateDate(date);
-populateDates();
+populateDates(selected_month,selected_year);
 
 date_picker_element.addEventListener('click',toggleDatePicker);
 next_mth_element.addEventListener('click',gotToNextMonth);
@@ -46,7 +47,7 @@ function gotToNextMonth(){
 		year++;
 	}
 	mth_element.textContent =  months[month] + ' ' + year;
-	populateDates();
+	populateDates(month,year);
 
 }
 
@@ -57,7 +58,7 @@ function goToPrevMonth(){
 		year--;
 	}
 	mth_element.textContent =  months[month] + ' ' + year;
-	populateDates();
+	populateDates(month,year);
 
 }
 
@@ -86,33 +87,40 @@ function formateDate(d){
 	return day + '/' + month + '/' + year;
 }
 
-function populateDates(){
+function populateDates(currentMonth,currentYear){
     days_element.innerHTML = '';
-	let amount_days = 31;
+    let firstDay = (new Date(currentYear, currentMonth)).getDay();
+for(let j=0; j<daysOfMonth.length ; j++){
+	const days_of_week = document.createElement('div');
+	days_of_week.textContent = daysOfMonth[j];
+	days_element.appendChild(days_of_week);
+}
 
-	if(month == 1){
-		amount_days = 28;
-	}
-
-	for (let i=0; i < amount_days; i++){
-		const day_element = document.createElement('a');
+let date = 1;
+	for (let i=0; i < daysInMonth(currentMonth, currentYear)+firstDay; i++){
+		const day_element = document.createElement('div');
 		day_element.classList.add('day');
-		 day_element.classList.add('day'+i);
-		day_element.textContent = i+1;
-		if(selected_day == (i+1) && selected_month == month && selected_year == year){
+		if(i < firstDay){
+           day_element.textContent = "";
+		}else{
+			// if(i == firstDay){
+			// 	day_element.tabIndex = 0;
+			// }
+			day_element.textContent = date;
+			date++;
+		}
+		
+		if(selected_day == (date -1 ) && selected_month == month && selected_year == year){
 			day_element.classList.add('selected');
 		}
-		if(i==0){
-			day_element.tabIndex = 0;
-		}
-           day_element.addEventListener('click', function(){
-           	selectedDate = new Date( year + '-' + (month+1)+ '-' + (i+1) );
-           	selected_day= i+1;
+           day_element.addEventListener('click', function(e){
+           	selectedDate = new Date( year + '-' + (month+1)+ '-' + (e.target.textContent) );
+           	selected_day= e.target.textContent ;
            	selected_month = month;
            	selected_year = year;
            	selected_date_element.textContent = formateDate(selectedDate);
            	selected_date_element.dataset.value = selectedDate;
-           	populateDates();
+           	populateDates(selected_month,selected_year);
 
            });
 
@@ -148,9 +156,73 @@ prev_mth_element.onkeydown = function(e){
             goToPrevMonth();
             break;
     }
-
 };
+days_element.onkeydown = function(e){
+	let allElements = days_element.children;
+	if(reqIndex == undefined){
+		for (let i = 0; i < allElements.length; i++) {
+    if (allElements[i].className == "day selected") {
 
+    	previousSelectedDate = i;
+      reqIndex=i;
+      break;
+    }        
+}
+	}
+
+
+switch (e.keyCode) {
+	case 13:
+	selected_date_element.textContent = "";	
+		selected_date_element.textContent = formateDate(new Date( selected_year + '-' + (selected_month+1)+ '-' + (reqIndex - 6-(new Date(selected_year, selected_month)).getDay()) ));
+           //	selected_date_element.dataset.value = new Date( selected_year + '-' + (selected_year+1)+ '-' + (e.target.textContent) );
+	allElements[previousSelectedDate].classList.remove('selected');
+	allElements[reqIndex].classList.add('selected');
+	previousSelectedDate=reqIndex;
+	break;
+	case 37: 
+	if(allElements[reqIndex-8] != undefined){
+		allElements[reqIndex].classList.remove('move');
+	allElements[reqIndex-1].classList.add('move');
+	reqIndex=reqIndex-1;
+	}
+	
+	break;
+	case 38: 
+	if(reqIndex >= 14){
+			allElements[reqIndex].classList.remove('move');
+	allElements[reqIndex-7].classList.add('move');
+	reqIndex=reqIndex-7;
+	}
+
+	break;
+	case 39: 
+	if(allElements[reqIndex+1] != undefined){
+		    allElements[reqIndex].classList.remove('move');
+	allElements[reqIndex+1].classList.add('move');
+	reqIndex = reqIndex+1;
+	}
+ 
+	break;
+	case 40:
+	if(allElements[reqIndex+7] != undefined){
+			allElements[reqIndex].classList.remove('move');
+	allElements[reqIndex+7].classList.add('move');
+	reqIndex=reqIndex+7;
+	}
+ 
+	break;
+	}
+
+
+
+
+}
+
+function daysInMonth(iMonth, iYear) 
+{ 
+    return 32 - new Date(iYear, iMonth, 32).getDate();
+}
 
 
 
